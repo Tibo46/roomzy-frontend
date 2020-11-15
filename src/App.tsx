9b474ab534fce2ad91d0e974c8e4151c0e49a2a1
lacant <thibaud.lacan@guestline.com>
2020-11-15 13:17:14 +0000

    Add environment variables for firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { db } from 'services/firebase';
 
 const App = () => {
-  console.log(process.env.REACT_APP_TEST);
   db.collection('test')
     .doc('iQyRNrz8ULPE08PX37q4')
     .onSnapshot({
