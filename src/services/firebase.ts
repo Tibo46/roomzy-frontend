8dab9b4bd67809bf8afffa99a466d5e0a6dfc6e6
lacant <thibaud.lacan@guestline.com>
2020-11-15 12:30:04 +0000

    Add environment variables and MUI package

---
+++ src/services/firebase.ts
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+};
+
+firebase.default.initializeApp(config);
+
+export const auth = firebase.default.auth;
+export const db = firebase.default.firestore();
