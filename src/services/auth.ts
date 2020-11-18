9fdf07264190f38469001889c77ef350d0b65394
henrypye@outlook.com <henrypye@outlook.com>
2020-11-18 13:33:11 +0100

    Tidied up auth providers

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -4,16 +4,28 @@ import { auth } from './firebase';
 export const login = (email: string, password: string) => {
   return auth.signInWithEmailAndPassword(email, password);
 };
+
 export const register = (email: string, password: string) => {
   return auth.createUserWithEmailAndPassword(email, password);
 };
+
 export const socialSignIn = (socialNetwork: 'facebook' | 'google') => {
-  return auth.signInWithPopup(
-    socialNetwork === 'google'
-      ? new firebase.auth.GoogleAuthProvider()
-      : new firebase.auth.FacebookAuthProvider()
-  );
+  let provider: firebase.auth.AuthProvider;
+
+  switch (socialNetwork) {
+    case 'google':
+      provider = new firebase.auth.GoogleAuthProvider();
+      break;
+    case 'facebook':
+      provider = new firebase.auth.FacebookAuthProvider();
+      break;
+    default:
+      throw Error();
+  }
+
+  return auth.signInWithPopup(provider);
 };
+
 export const logout = () => {
   auth.signOut();
 };
